91e0e1237ab433723df7a6c97a1a22d3f61a01cb
Andre335 <andre335@gmail.com>
2014-09-18 03:05:49 -0300

    algo besta

--- src/Fonte/Banda.java
+++ src/Fonte/Banda.java
@@ -4,7 +4,10 @@ public class Banda {
 
 	private String nome;
 
-	public Banda(String nome) {
+	public Banda(String nome) throws Exception {
+		if (nome == null)
+			throw new Exception("Nome invalido!");
+		
 		this.nome = nome;
 	}
 
